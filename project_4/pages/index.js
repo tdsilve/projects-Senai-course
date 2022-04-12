61e8689f74a1a1ab7097341f560906a3a547db38
Thais Oliveira <thaidsilve@gmail.com>
2022-04-12 00:16:40 -0300

    project 4 - add a new task at local storage when click eht button Adicionar Tarefa

--- project_4/pages/index.js
+++ project_4/pages/index.js
@@ -2,18 +2,35 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 
 export default function Home() {
-  const [todoValue, setTodoValue] = useState('');
+  const [todoValue, setTodoValue] = useState("");
   const [todoList, setTodoList] = useState([]);
+
+  // Save the task at local storage
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const todoListItem = localStorage.getItem("todoListItem");
+
+      try{
+        console.log(JSON.parse(todoListItem));
+      }catch(e){
+        window.localStorage.setItem("todoListItem", JSON.stringify([]))
+      }
+    }
+  }, []);
+
+  //Save a list at local storage everytime the user add a new task
+  useEffect(() => {
+    window.localStorage.setItem('todoListItem', JSON.stringify(todoList));
+  }, [todoList]);
 
   const handleButtonClick = () => {
     setTodoList([...todoList, todoValue]);
-    setTodoValue('');
-  }
-
+    setTodoValue("");
+  };
 
   return (
     <>
@@ -27,16 +44,33 @@ export default function Home() {
             onChange={(e) => setTodoValue(e.target.value)}
           />
           {/* Disable Adicionar Tarefa if the user has not write anything */}
-          { todoValue ? 
-            <button type="submit" className="btn btn-primary mt-3" onClick={handleButtonClick}>
-            Adicionar Tarefa
-          </button> :
-            <button type="submit" className="btn btn-primary mt-3" onClick={handleButtonClick} disabled>
-            Adicionar Tarefa
-          </button> 
-          }
+          {/* {todoValue ? (
+            <button
+              type="submit"
+              className="btn btn-primary mt-3"
+              onClick={handleButtonClick}
+            >
+              Adicionar Tarefa
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="btn btn-primary mt-3"
+              onClick={handleButtonClick}
+              disabled
+            >
+              Adicionar Tarefa
+            </button>
+          )} */}
+             <button
+              type="submit"
+              className="btn btn-primary mt-3"
+              onClick={handleButtonClick}
+            >
+              Adicionar Tarefa
+            </button>
         </div>
-        <TodoList list={todoList}/>
+        <TodoList list={todoList} />
       </div>
     </>
   );
